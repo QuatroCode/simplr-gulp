9ede7e2213b459def1c048d97f36143494da193c
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-06-10 13:28:23 +0300

    Removed example old. Installed npm-tsconfig-lint in example and typings.

--- example/example_old/src/app/test.js
+++
@@ -1,8 +0,0 @@
-var TestClass = (function () {
-    function TestClass() {
-        console.log("test.ts: TestClass() init.");
-    }
-    return TestClass;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = TestClass;
