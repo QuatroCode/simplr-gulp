215ca581c2340f7f97a463aec356fe786482a2f0
Giedrius Grabauskas <giedrius@quatrodev.com>
2016-07-19 17:44:47 +0300

    Added comments.

--- src/tasks/task-base.ts
+++ src/tasks/task-base.ts
@@ -1,7 +1,14 @@
 import { Task } from './tasks-contracts';
 
+/**
+ * Abstract task base
+ * 
+ * @abstract
+ * @class TaskBase
+ * @implements {Task}
+ */
 abstract class TaskBase implements Task {
-    abstract Name: string;
+    public abstract Name: string;
     public abstract TaskFunction(production: boolean, done?: Function): void | NodeJS.Process | any;
 }
 
