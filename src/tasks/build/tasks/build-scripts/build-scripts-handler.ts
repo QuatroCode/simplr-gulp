e76a36938b6d167c6692ab5fb1acbc64aa56d86a
Dovydas Navickas <dovydas@quatrodev.com>
2017-02-28 00:35:58 +0200

    TSLint tasks removed.

--- src/tasks/build/tasks/build-scripts/build-scripts-handler.ts
+++ src/tasks/build/tasks/build-scripts/build-scripts-handler.ts
@@ -3,14 +3,13 @@ import { Task } from '../../../task-base';
 
 //Tasks
 import BuildTypescript from './tasks/build-scripts-typescript-task';
-import BuildTslint from './tasks/build-scripts-tslint-task';
 
 export default class Tasks extends TasksHandler<Task> {
 
     constructor() {
         super(config => {
             config.Name = "Build.Scripts";
-            config.Tasks = [BuildTypescript, BuildTslint];
+            config.Tasks = [BuildTypescript];
             config.WithProduction = true;
             config.TasksAsync = true;
             return config;
