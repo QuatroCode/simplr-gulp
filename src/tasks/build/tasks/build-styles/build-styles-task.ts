fce6cdce9322ed72a07fb8c421fd737ef2b9d784
Dovydas Navickas <dovydas@quatrodev.com>
2017-03-01 23:13:49 +0200

    Builder initialization moved from constructor after super to property initializer.

--- src/tasks/build/tasks/build-styles/build-styles-task.ts
+++ src/tasks/build/tasks/build-styles/build-styles-task.ts
@@ -4,15 +4,13 @@ import { StylesBuilder } from '../../../../builders/styles/styles-builder';
 export default class BuildStylesgTask extends TaskBase {
     constructor() {
         super();
-        this.Builder = new StylesBuilder();
     }
 
-    Builder: StylesBuilder;
+    Builder: StylesBuilder = new StylesBuilder();;
 
     Name = "Build.Styles";
 
     Description = "Compiles *.scss files from source to build directory";
 
     TaskFunction = this.Builder.Build;
-
 }
